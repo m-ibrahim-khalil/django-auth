783992e211432f47ea965581e173a17d2bb66d52
m-ibrahim-khalil <mik858692@gmail.com>
2023-12-21 20:29:22 +0600

    finished authentication

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 app_name = "myapp"
@@ -7,9 +8,11 @@ def index(request):
     return render(request, "myapp/index.html")
 
 
+@login_required
 def profile(request):
     return render(request, "myapp/profile.html")
 
 
+@login_required
 def manage_account(request):
     return render(request, "myapp/manage_account.html")
